cee5bc28cabfdf1510d288d93b675f76af087ee2
unknown <dbp1795@aim.com>
2022-12-05 12:30:24 -0500

    connected api to all dates

--- assets/script.js
+++ assets/script.js
@@ -2,6 +2,23 @@ var searchBtn = document.getElementById('button');
 var humidEl = document.getElementById('humidity');
 var windEl = document.getElementById('wind');
 var tempEl= document.getElementById('temperature');
+var temp1 = document.getElementById('temperature-0');
+var temp2 = document.getElementById('temperature-1');
+var temp3 = document.getElementById('temperature-2');
+var temp4 = document.getElementById('temperature-3');
+var temp5 = document.getElementById('temperature-4');
+var hum1 = document.getElementById('humidity-0');
+var hum2 = document.getElementById('humidity-1');
+var hum3 = document.getElementById('humidity-2');
+var hum4 = document.getElementById('humidity-3');
+var hum5 = document.getElementById('humidity-4');
+var wind1 = document.getElementById('wind-0');
+var wind2 = document.getElementById('wind-1');
+var wind3 = document.getElementById('wind-2');
+var wind4 = document.getElementById('wind-3');
+var wind5 = document.getElementById('wind-4');
+
+
 
 
 
@@ -12,12 +29,31 @@ fetch(requestUrl)
     return response.json()
 }).then(function(data) {
   console.log(data)
+
   tempEl.textContent= data.list[0].main.temp
   windEl.textContent= data.list[0].wind.speed
   humidEl.textContent= data.list[0].main.humidity
+  temp1.textContent= data.list[1].main.temp
+  temp2.textContent= data.list[2].main.temp
+  temp3.textContent= data.list[3].main.temp
+  temp4.textContent= data.list[4].main.temp
+  temp5.textContent= data.list[5].main.temp
+  hum1.textContent = data.list[1].main.humidity
+  hum2.textContent = data.list[2].main.humidity
+  hum3.textContent = data.list[3].main.humidity
+  hum4.textContent = data.list[4].main.humidity
+  hum5.textContent = data.list[5].main.humidity
+  wind1.textContent = data.list[1].wind.speed
+  wind2.textContent = data.list[2].wind.speed
+  wind3.textContent = data.list[3].wind.speed
+  wind4.textContent = data.list[4].wind.speed
+  wind5.textContent = data.list[5].wind.speed
 });
 
 }
+
+
+
 
 function getWeather(event){
   event.preventDefault()
@@ -30,4 +66,6 @@ function getWeather(event){
   callApi(location);
 
 }
+
+
 searchBtn.addEventListener('click',getWeather)
